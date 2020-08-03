cdbe08fc99b7ac123fdaee62c21fc475e1f8b817
Matt Debnar <matthew.debnar@gmail.com>
2020-08-03 01:23:47 -0400

    added comments, index card style, pandas Bootstrap table class format

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -9,14 +9,17 @@ from time import sleep
 
 def init_browser():
     # @NOTE: Replace the path with your actual path to the chromedriver
-    #windows work-around for chromedriver.exe 'e' error
+    #Splinter Windows work-around for chromedriver.exe 'e' error
     executable_path = {'executable_path': ChromeDriverManager().install()}
+    #Splinter browser setup for MacOS
+    #executable_path = {'executable_path': '/usr/local/bin/chromedriver'}
     return Browser("chrome", **executable_path, headless=False)
 
 
 def scrape():
     browser = init_browser()
 
+    # NASA MARS NEWS
     # Visit Nasa Mars News
     news_url = 'https://mars.nasa.gov/mars2020/news/'
     # Scrape page into Soup
@@ -26,47 +29,66 @@ def scrape():
     #define news_title and news_p to render
     news_title = news_soup.find("div", class_='listTextLabel').find('h2', class_='alt01').text.strip()
     news_p = news_soup.find("div", class_='listTextLabel').find('p').text.strip()
-    # Store data in the mars_data dictionary
+    
+    # Store NEWS data in the mars_data dictionary
     mars_data = {
         "news_title": news_title,
         "news_p": news_p
      }
 
 
-    #get headline image
+    #JPL MARS SPACE IMAGES - FEATURED IMAGE
     sleep(1)
+    #JPL Mars Space Images URL
     img_url = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
+    #browse to page
     browser.visit(img_url)
     img_html = browser.html
     img_soup = bs(img_html, 'html.parser')
+    #featured image is in the carousel item class, style section; parse by single quote and take second element for url
     imgs = img_soup.find(class_ = "carousel_item")['style']
     featured_image = imgs.split("'")[1]
+    #add the domain as the base to get the full URL
     img_base_url = 'https://www.jpl.nasa.gov'
     featured_image_url = img_base_url + featured_image
 
+    # Store FEATURED IMAGE URL data in the mars_data dictionary
     mars_data.update( {'featured_image_url' : featured_image_url} )
 
-    #get the Mars Facts and nothing but the facts
+
+    #MARS FACTS
+    # get the Mars Facts and nothing but the facts
     sleep(1)
+    #set the url and use pandas to pull table
     facts_url = 'https://space-facts.com/mars/'
     facts_url_tables = pd.read_html(facts_url)
+    #pull the first table on the page into a data frame
     mars_facts_df = facts_url_tables[0]
-    mars_facts_df.columns = ['Label', 'Value']
+    mars_facts_df.columns = ['Label', 'Mars']
     mars_facts_df.set_index('Label', inplace=True)
-    html_table = mars_facts_df.to_html()
+    #drop index.name to avoid funky second index header row
+    mars_facts_df.index.name=None
+    #format for subsequent HTML display via Bootstrap CSS
+    html_table = mars_facts_df.to_html(classes = 'table table-striped table-hover')
 
+    # Store MARS FACTS html table data in the mars_data dictionary
     mars_data.update( {'html_table' : html_table} )
 
-    #get Mars Hemisphere images
+    #MARS HEMISPHERE IMAGES
+    # get four Mars Hemisphere images and store in Mongo array
     sleep(1)
+    #set the URL and request
     hemi_url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
     browser.visit(hemi_url)
+    #parse the landing page, isolate the image links in the item div
     hemi_html = browser.html
     hemi_soup = bs(hemi_html, "html.parser")
     hemi_links = hemi_soup.find_all("div", class_ = "item")
 
+    #create a list for the names and image links
     hemi_img_urls = []
 
+    #loop thru each link and grab the name and full size image
     for link in hemi_links:
         #dictionary to hold the name/link pairs
         hemi_dict = {}
@@ -95,8 +117,10 @@ def scrape():
         hemi_dict['hemi_img_url'] = link_base + indv_hemi_url
         #put dictionary into hemi_img_urls
         hemi_img_urls.append(hemi_dict)
+        #back up to repeat
         browser.back()
 
+    # Store MARS HEMISPHERE IMAGES data in the mars_data dictionary as an array
     mars_data.update( {'hemi_img_urls': hemi_img_urls} )
 
     # Close the browser after scraping
